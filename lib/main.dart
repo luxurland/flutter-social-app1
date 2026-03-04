06477c882ac92f07caa64402822734cf72dcf06c
Mahmoud <mod.mhsn@gmail.com>
2026-03-04 12:28:12 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,35 @@
 import 'package:flutter/material.dart';
 import 'api/api_service.dart';
-import 'screens/home_screen.dart';
-import 'screens/store_screen.dart';
-import 'screens/profile_screen.dart';
+import 'theme.dart';
+import 'app_shell.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  final api = ApiService("https://1.mod-mhsn.workers.dev/");
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final api = ApiService("https://YOUR_WORKER_URL");
+  bool darkMode = false;
+
+  void toggleTheme() {
+    setState(() {
+      darkMode = !darkMode;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Social App",
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.blue,
-      ),
+      theme: darkMode ? AppTheme.dark : AppTheme.light,
       home: AppShell(api: api),
-    );
-  }
-}
-
-class AppShell extends StatefulWidget {
-  final ApiService api;
-  const AppShell({super.key, required this.api});
-
-  @override
-  State<AppShell> createState() => _AppShellState();
-}
-
-class _AppShellState extends State<AppShell> {
-  int index = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    final screens = [
-      HomeScreen(api: widget.api),
-      StoreScreen(api: widget.api),
-      ProfileScreen(api: widget.api),
-    ];
-
-    return Scaffold(
-      body: screens[index],
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: index,
-        onDestinationSelected: (i) => setState(() => index = i),
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.home_outlined),
-            selectedIcon: Icon(Icons.home),
-            label: "Home",
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.store_outlined),
-            selectedIcon: Icon(Icons.store),
-            label: "Store",
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.person_outline),
-            selectedIcon: Icon(Icons.person),
-            label: "Profile",
-          ),
-        ],
-      ),
     );
   }
 }
