432a722c2a6c553b4d0374e387da17d0b666ead6
Mahmoud <mod.mhsn@gmail.com>
2026-03-04 19:14:07 +0000

    Create app_routes.dart

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,10 @@
+class AppRoutes {
+  static const store = "/store";
+  static const products = "/products";
+  static const personalFeed = "/personal-feed";
+  static const productFeed = "/product-feed";
+  static const reports = "/reports";
+  static const callLobby = "/call-lobby";
+  static const callActive = "/call-active";
+  static const callHistory = "/call-history";
+}
