45cfaa671e28d11f21288427ded94cfb8d577104
Mahmoud <mod.mhsn@gmail.com>
2026-03-04 20:06:42 +0000

    Update store_controller.dart

--- lib/features/store/controller/store_controller.dart
+++ lib/features/store/controller/store_controller.dart
@@ -1,22 +1,22 @@
 import 'package:get/get.dart';
-import '../data/store_repository.dart';
+import '../../../api/api_service.dart';
 
 class StoreController extends GetxController {
-  final StoreRepository repo;
-  StoreController(this.repo);
+  final ApiService api = Get.find();
 
-  var store = Rxn<Map<String, dynamic>>();
+  var products = [].obs;
   var loading = false.obs;
 
-  Future<void> loadMyStore() async {
-    loading.value = true;
-    store.value = await repo.myStore();
-    loading.value = false;
+  @override
+  void onInit() {
+    super.onInit();
+    loadProducts();
   }
 
-  Future<int> create(String name, String description) async {
-    final id = await repo.createStore(name, description);
-    await loadMyStore();
-    return id;
+  Future<void> loadProducts() async {
+    loading.value = true;
+    final res = await api.get("store/products");
+    products.value = res;
+    loading.value = false;
   }
 }
