c250f78eb7dfb4959524dba1a9c300202a16a58b
Mahmoud <mod.mhsn@gmail.com>
2026-03-04 20:02:39 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'api/api_service.dart';
 import 'theme.dart';
 import 'app_shell.dart';
 import 'routes/app_pages.dart';
@@ -10,36 +9,19 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final api = ApiService("https://1.mod-mhsn.workers.dev/");
-  bool darkMode = false;
-
-  void toggleTheme() {
-    setState(() {
-      darkMode = !darkMode;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: "Social App",
-      theme: darkMode ? AppTheme.dark : AppTheme.light,
-
-      // ❗ لا نستخدم home + initialRoute معًا
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: ThemeMode.system,
       initialRoute: AppRoutes.home,
-
       getPages: AppPages.pages,
-
-      // ❗ نحذف home: AppShell(api: api)
-      // لأن AppShell سيتم فتحه عبر Route /home
+      home: AppShell(),
     );
   }
 }
