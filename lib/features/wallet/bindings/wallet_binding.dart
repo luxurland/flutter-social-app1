b4fa88c3b8d131095bd4885ed887969691268fdf
Mahmoud <mod.mhsn@gmail.com>
2026-03-04 19:59:11 +0000

    Create wallet_binding.dart

---
+++ lib/features/wallet/bindings/wallet_binding.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import '../controller/wallet_controller.dart';
+
+class WalletBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => WalletController());
+  }
+}
