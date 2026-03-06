490465beac7bbb0a961f3135aadf783d9264799d
Mahmoud <mod.mhsn@gmail.com>
2026-03-06 00:17:16 +0000

    Implement secure login with password hashing
    Refactored the login function to include password hashing for improved security.

--- worker/auth/login.js
+++ worker/auth/login.js
@@ -1,22 +1,3 @@
-// import { json, error } from "../utils/response.js";
-// import { signJWT } from "../utils/jwt.js";
-
-// export async function login(request, env) {
-//   const { username, password } = await request.json();
-
-//   const user = await env.db1.prepare(
-//     "SELECT * FROM users WHERE username = ? AND password = ?"
-//   ).bind(username, password).first();
-
-//   if (!user) return error("Invalid credentials");
-
-//   const token = signJWT({ id: user.id, username }, env.JWT_SECRET);
-
-//   return json({ token });
-// }
-
-
-// /flutter-social-app1/worker/auth/login.js
 import { json, error } from "../utils/response.js";
 import { signJWT } from "../utils/jwt.js";
 import bcrypt from 'bcryptjs';
